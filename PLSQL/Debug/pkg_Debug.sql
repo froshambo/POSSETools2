94cde0b6476bd11d21cba6749636401b0668f762
froshambo <tribpickle@gmail.com>
2015-02-18 23:49:02 -0700

    Initial commit

---
+++ PLSQL/Debug/pkg_Debug.sql
@@ -0,0 +1,13 @@
+create or replace package pkg_Debug as
+	/* Standard methods for outputting debug and other logging messages. */
+
+
+end pkg_Debug;
+/
+
+create or replace package body pkg_Debug as
+	/* Standard methods for outputting debug and other logging messages. */
+
+	
+end pkg_Debug;
+/
