e8b25caee2d0e0e5c08a9aa314df71b8bbd35c78
froshambo <tribpickle@gmail.com>
2015-02-19 00:01:29 -0700

    Added early method descriptions to package header

--- PLSQL/Debug/pkg_Debug.sql
+++ PLSQL/Debug/pkg_Debug.sql
@@ -1,7 +1,15 @@
 create or replace package pkg_Debug as
-	/* Standard methods for outputting debug and other logging messages. */
+	/* Standard methods for outputting debug and other logging messages. 
+	   Supports logging to file and to console.*/
 
+    -- BeginOutput: Write header information (name of method, parameters, etc)
 
+    -- EndOutput: Write footer information (return values, success/failure)
+
+    -- WriteLine: Write a line of output
+
+    -- WriteToFile
+    -- WriteToConsole
 end pkg_Debug;
 /
 
